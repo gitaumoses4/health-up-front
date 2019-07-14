af37612372d3b886c48cbee7f83819cbef39b554
Moses Gitau <gitaumoses4@gmail.com>
2019-07-14 11:33:33 +0300

    Add deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,36 @@
 #!/usr/bin/env bash
 yarn build
 
-find ./build/ -exec curl -T {} ftp://ftp.healthupsa.com/root/{} -u qcitteam751022:UP123up! --ftp-create-dirs \;
+cd ./build/
+
+function createHost(){
+    host="ftp://ftp.healthupsa.com/$1 --user qcitteam751022:UP123up!";
+}
+export -f createHost
+
+function uploadFiles(){
+    if [[ ${1} != "." ]]
+    then
+        createHost ""
+        curl $(echo ${host} ) -Q "MKD $1";
+    fi
+
+    cd $1
+    pwd
+    for f in $(find . -maxdepth 1 -type f)
+    do
+        echo ${f}
+        fileName=$(echo ${f} | cut -c 2-)
+        createHost $(echo "$1${fileName}" | cut -c 3-)
+        curl $(echo ${host}) -T ${f}
+    done
+
+    if [[ ${1} != "." ]]
+    then
+        cd ..
+    fi
+}
+
+export -f uploadFiles
+
+find . -type d -exec bash -c 'uploadFiles "$0"' {} \;
